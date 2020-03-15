d29d351d47821627416233d9fb1b7ba0f93af5a4
highwayns <zhengjun@jp.highwayns.com>
2020-03-16 08:23:10 +0900

    my first commit

---
+++ pages/terms.js
@@ -0,0 +1,20 @@
+import { Card, Layout, Page } from '@shopify/polaris';
+
+const Terms = () => (
+  <Page>
+    <Layout>
+      <Layout.Section oneHalf>
+        <Card>
+          <div>Put content here</div>
+          <a href="https://polaris.shopify.com/components/structure/layout">See Polaris docs</a>
+        </Card>
+      </Layout.Section>
+      <Layout.Section oneHalf>
+        <Card>
+          Put content here
+        </Card>
+      </Layout.Section>
+    </Layout>
+  </Page >
+  );
+  export default Terms;
